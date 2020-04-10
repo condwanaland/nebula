1081fb2a57cff6e3a88351b62d40f175f40b72ea
Conor Neilson <condwanaland@gmail.com>
2020-04-10 22:12:38 +1200

    progress

--- inst/app/app.R
+++ inst/app/app.R
@@ -7,7 +7,6 @@ library(colourpicker)
 library(shinyWidgets)
 library(nebula)
 
-
 # Define UI
 ui <- fluidPage(
 
@@ -44,11 +43,11 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output, session) {
 
-  # Set up some necessary housekeeping
-  # Create reactive dataframe to store clicks in
-  click_data_reactive <- reactiveValues()
+  # Create a reactive dataframe to store plot clicks in
+  click_data_reactive <- shiny::reactiveValues()
   click_data_reactive$click_data <- create_empty_df("x_values", "y_values")
-  output_data <- reactive({click_data_reactive$click_data})
+  output_data <- shiny::reactive({click_data_reactive$click_data})
+
 
   # Create a table to display. output_data is in its own expression so it can be used in the
   # download handler
@@ -61,8 +60,6 @@ server <- function(input, output, session) {
   loaded_image <- eventReactive(input$current_image, {
     magick::image_read(input$current_image$datapath)
   })
-
-
 
   # Handle the image output, and click observations.
   # Would like to modularise this - not sure how currently.
@@ -90,9 +87,6 @@ server <- function(input, output, session) {
   })
 
   # Observe remove button
-  # observeEvent(input$delete_point, {
-  #   remove_last_row(click_data_reactive)
-  # })
   callModule(deletePoint,
              "delete_point",
              click_data_reactive = click_data_reactive)
@@ -101,7 +95,7 @@ server <- function(input, output, session) {
   # Handle the reset all modal. This is done in a 2-pass manner The first module starts a
   # modal with a 'confirm' button. The confirm button then triggers the app reset.
   callModule(resetButton, "resetAll")
-  callModule(confirmDelete, "confirmDelete")
+  callModule(confirmDelete, "confirmDelete", click_data_rm = click_data_reactive$click_data)
 
   # Data download module
   callModule(downloadData, id = "downloadData",
